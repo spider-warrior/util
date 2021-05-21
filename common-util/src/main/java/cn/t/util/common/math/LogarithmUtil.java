02a947c9601192280853c2232e3a09ca918772c5
yangjian <362961910@qq.com>
2021-05-21 16:53:33 +0800

    完善 注解使用 优化 配置

--- common-util/src/main/java/cn/t/util/common/math/LogarithmUtil.java
+++ common-util/src/main/java/cn/t/util/common/math/LogarithmUtil.java
@@ -6,6 +6,7 @@ public class LogarithmUtil {
      * 换底公式: logx(y) = loge(y) / loge(x)
      * @param value 计算的值
      * @param base 底
+     * @return value
      * */
     public static double log(double value, double base) {
         return Math.log(value) / Math.log(base);
@@ -17,6 +18,7 @@ public class LogarithmUtil {
      * @author   yj
      * @param value 底数
      * @param base 指数
+     * @return value
      */
     public static int logDown(double value, double base) {
         return (int)log(value, base);
@@ -26,6 +28,7 @@ public class LogarithmUtil {
      * 求指数(以顶为准)
      * @param value 底数
      * @param base 指数
+     * @return value
      */
     public static int logUp(double value, double base) {
         return (int) Math.ceil(log(value, base));
