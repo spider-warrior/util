260ffe9b1c97f6b6d2437081a1da46719c5d3254
yangjian <362961910@qq.com>
2022-11-15 14:16:34 +0800

    update CertificateGenerateTest.java

--- security-util/src/test/java/security/ssl/CertificateGenerateTest.java
+++ security-util/src/test/java/security/ssl/CertificateGenerateTest.java
@@ -13,19 +13,23 @@ import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.junit.Test;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
-import java.security.PrivateKey;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.Enumeration;
 
 /**
  * 参考地址
  * <a href="https://gamlor.info/posts-output/2019-10-29-java-create-certs-bouncy/en/">Create HTTPS Certificates in Java with Bouncy Castle</a>
+ * 1.将issuer证书使用mmc导入到信任的证书办法机构
+ * 2.p12密钥库提取证书和私钥配置在nginx
+ * openssl pkcs12 -in abc-cert.p12 -out abc.key -nodes -nocerts
+ * openssl pkcs12 -in abc-cert.p12 -out abc.crt -nokeys
  */
 public class CertificateGenerateTest {
 
@@ -40,26 +44,57 @@ public class CertificateGenerateTest {
 
     @Test
     public void generateTest() throws Exception {
-        GeneratedCert rootCA = createCertificate("do_not_trust_test_certs_root",   /*domain=*/null,     /*issuer=*/null,  /*isCa=*/true);
-        GeneratedCert issuer = createCertificate("do_not_trust_test_certs_issuer", /*domain=*/null,     rootCA,           /*isCa=*/true);
-        GeneratedCert domain = createCertificate("local.gamlor.info",              "local.gamlor.info", issuer,           /*isCa=*/false);
-        GeneratedCert otherD = createCertificate("other.gamlor.info",              "other.gamlor.info", issuer,           /*isCa=*/false);
+        GeneratedCert rootCrt = createCertificate("do_not_trust_test_certs_root",   /*issuer=*/null,  /*isCa=*/true);
+        GeneratedCert issuerCrt = createCertificate("do_not_trust_test_certs_issuer", rootCrt,           /*isCa=*/true);
+        GeneratedCert domainCrt = createCertificate("abc cert",                       issuerCrt,           /*isCa=*/false,              "abc.com", "*.abc.com");
         char[] emptyPassword = new char[0];
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
         // Key store expects a load first to initialize.
         keyStore.load(null, emptyPassword);
         // Store our domain certificate, with the private key and the cert chain
-        keyStore.setKeyEntry("local.gamlor.info", domain.privateKey, emptyPassword,
-            new X509Certificate[]{domain.certificate, issuer.certificate, rootCA.certificate});
-        keyStore.setKeyEntry("other.local.gamlor.info", otherD.privateKey, emptyPassword,
-            new X509Certificate[]{otherD.certificate, issuer.certificate, rootCA.certificate});
-        // Store to a file
-        try (FileOutputStream store = new FileOutputStream("D:\\tmp\\ssl\\java-ssl\\my-cert.pfx")) {
-            keyStore.store(store, emptyPassword);
+        persistKeyStore(
+            keyStore,
+            emptyPassword,
+            "root-crt", rootCrt,
+            new X509Certificate[]{rootCrt.certificate},
+            "D:\\tmp\\ssl\\java-ssl\\root-cert.p12"
+        );
+        clearKeyStore(keyStore);
+        persistKeyStore(
+            keyStore,
+            emptyPassword,
+            "issuer-crt", issuerCrt,
+            new X509Certificate[]{issuerCrt.certificate, rootCrt.certificate},
+            "D:\\tmp\\ssl\\java-ssl\\issuer-cert.p12"
+        );
+        clearKeyStore(keyStore);
+        persistKeyStore(
+            keyStore,
+            emptyPassword,
+            "abc-crt",
+            domainCrt,
+            new X509Certificate[]{domainCrt.certificate, issuerCrt.certificate, rootCrt.certificate},
+            "D:\\tmp\\ssl\\java-ssl\\abc-cert.p12"
+        );
+        clearKeyStore(keyStore);
+    }
+
+    private void clearKeyStore(KeyStore keyStore) throws KeyStoreException {
+        Enumeration<String> aliases = keyStore.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            keyStore.deleteEntry(alias);
         }
     }
 
-    private static GeneratedCert createCertificate(String cnName, String domain, GeneratedCert issuer, boolean isCA) throws Exception {
+    private void persistKeyStore(KeyStore keyStore, char[] password, String alias, GeneratedCert generatedCert, X509Certificate[] chain, String path) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        keyStore.setKeyEntry(alias, generatedCert.privateKey, password, chain);
+        try (FileOutputStream os = new FileOutputStream(path)) {
+            keyStore.store(os, password);
+        }
+    }
+
+    private static GeneratedCert createCertificate(String cnName, GeneratedCert issuer, boolean isCA, String... domains) throws Exception {
         // Generate the key-pair with the official Java API's
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         KeyPair certKeyPair = keyGen.generateKeyPair();
@@ -92,9 +127,13 @@ public class CertificateGenerateTest {
             builder.addExtension(Extension.basicConstraints, true, new BasicConstraints(isCA));
         }
         // Modern browsers demand the DNS name entry
-        if (domain != null) {
+        if (domains != null && domains.length > 0) {
+            GeneralName[] generalNames = new GeneralName[domains.length];
+            for (int i = 0; i < domains.length; i++) {
+                generalNames[i] = new GeneralName(GeneralName.dNSName, domains[i]);
+            }
             builder.addExtension(Extension.subjectAlternativeName, false,
-                new GeneralNames(new GeneralName(GeneralName.dNSName, domain)));
+                new GeneralNames(generalNames));
         }
         // Finally, sign the certificate
         ContentSigner signer = new JcaContentSignerBuilder("SHA256WithRSA").build(issuerKey);
