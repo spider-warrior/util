16195a62a8d002f8111b5f8e1d961396e00142bd
36296 <362961910@qq.com>
2023-03-23 10:23:10 +0800

    1.0.17.RELEASE

--- common-util/src/main/java/cn/t/util/common/RegexUtil.java
+++ common-util/src/main/java/cn/t/util/common/RegexUtil.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegexUtil {
 
-    private static final Pattern IP_MATCH_REG = Pattern.compile("^\\b([0-9]{1,3}\\.){3}[0-9]{1,3}\\b$");
+    private static final Pattern IP_MATCH_REG = Pattern.compile("^\\b(\\d{1,3}\\.){3}\\d{1,3}\\b$");
     private static final Pattern IP_REG = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
 
     private static final Pattern INSERT_SQL_TABLE_NAME_REG = Pattern.compile("into\\s+([1-9_a-z]+)");
